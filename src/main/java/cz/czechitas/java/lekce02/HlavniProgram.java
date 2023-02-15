c246f3e84c4b3bad1b6ddbc6ceb408255da50db1
TanaFed <krupik@tut.by>
2023-02-15 19:57:37 +0100

    zelva domecky

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,8 +7,42 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
-        //TODO tady bude kód
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnRight(90);
+
+        for (int i = 0; i < 3; i++) {
+            nakresliDomecek();
+        }
+        zofka.turnRight(90);
+        zofka.move(200);
+        zofka.turnRight(180);
+
     }
+
+    private void nakresliDomecek() {
+        zofka.penDown();
+        for (int dom = 0; dom < 4; dom++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+        zofka.penUp();
+        zofka.move(100);
+        zofka.penDown();
+        zofka.turnRight(45);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
+        zofka.penUp();
+
+        zofka.turnRight(45);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+    }
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
